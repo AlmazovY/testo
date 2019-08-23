7f4f8672ca885f3a4a292e84845d8fc072248553
Yakov <rutkarf@gmail.com>
2019-08-23 12:10:27 +0400

    Второе задание

---
+++ sanbox/src/main/java/exerciseTwo.java
@@ -0,0 +1,9 @@
+public class exerciseTwo {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(10, 20);
+        Point p2 = new Point(30, 30);
+        System.out.println("Расстояние между точками с координатами" + " (" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " равняется " + (p1, p2).distance);
+    }
+}
